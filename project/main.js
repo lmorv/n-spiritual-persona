3abcc9841057af67450c925d7d690db7f39afd24
lmorv <kishongus@gmail.com>
2023-11-03 12:01:28 -0400

    WebDev: Mesh display attempt
    - Attempting to set up a three.js scene with the test body bits.

---
+++ project/main.js
@@ -0,0 +1,43 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+// set up the scene
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+// set up the renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+
+// Display a cube:
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+const controls = new OrbitControls(camera, renderer.domElement);
+
+
+const models = []; // Array to hold models
+
+// loop to position and add models to the scene
+for (let i = 0; i < 10; i++) {
+  const model = `/fbx/body-bit-${i}.fbx`// Load your model 
+  model.position.set(0, 0, 0); // Set the position
+  models.push(model);
+  scene.add(model);
+}
+
+function animate() {
+	requestAnimationFrame( animate ); // refresh the renderer 
+
+    // rotate the cube
+    // cube.rotation.x += 0.01;
+	// cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+animate();
