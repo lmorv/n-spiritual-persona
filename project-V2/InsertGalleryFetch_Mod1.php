c78462029745bd0451ee8f9ad79239e5894d4144
lmorv <kishongus@gmail.com>
2023-12-04 20:13:23 -0500

    levelDesign: shaders and placement
    - Finalized voronoi terrain map texture, and adjusted a bunch of look related stuffs to make it all look pretty and well laid out. Added a couple farmlands and made them visualazable in the editor window with a square gizmo.

---
+++ project-V2/InsertGalleryFetch_Mod1.php
@@ -0,0 +1,244 @@
+<?php
+//check if there has been something posted to the server to be processed
+
+if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET["start"])) {
+//echo "hello";
+readFromFile();
+exit;
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+// need to process
+ $artist = $_POST['a_name'];
+ $terms = $_POST['terms'];
+ $mist = $_POST['mist'];
+ $color = $_POST['color'];
+ $rainbow = $_POST['rainbow'];
+ $ghost = $_POST['ghost'];
+ $clouds = $_POST['clouds'];
+ $boom = $_POST['boom'];
+ $plant = $_POST['plant'];
+
+
+ //package the data and echo back...
+    /* make  a new generic php object */
+    $myPackagedData=new stdClass();
+    // $myPackagedData->response = "success";
+    $myPackagedData->artist = $artist ;
+    $myPackagedData->aterms = $terms ;
+    $myPackagedData->amist = $mist ;
+    $myPackagedData->acolor = $color ;
+    $myPackagedData->arainbow = $rainbow ;
+    $myPackagedData->aghost = $ghost ;
+    $myPackagedData->aclouds = $clouds ;
+    $myPackagedData->aboom = $boom ;
+    $myPackagedData->aplant = $plant ;
+     /* Now we want to JSON encode these values as a JSON string ..
+     to send them to $.ajax success  call back function... */
+    $myJSONObj = json_encode($myPackagedData);
+    // echo $myJSONObj;
+
+// save the packaged data to a file
+$file = fopen("files/user_input.txt", "a") or die("unable to open file");
+fwrite($file, $myJSONObj . "\n");
+fclose($file);
+
+readFromFile();
+
+ exit;
+}//POST
+
+function readFromFile() {
+
+  $path = "files/user_input.txt";
+  $theFile = fopen($path, "r") or die("Unable to open file!");
+  //read until eof
+  //$i=0;
+  $outArr = array();
+  // $NUM_PROPS = 3; // number of fields (dependent on the format of the file. In this case 'animal','color', 'number')
+   //echo("test");
+     while(!feof($theFile)) {
+       //create an object to send back
+
+
+      $str = fgets($theFile);
+      $outArr[] = json_decode($str);
+     }
+
+     fclose($theFile);
+       // var_dump($outArr);
+       // Now we want to JSON encode these values to send back.
+      $myJSONObj = json_encode($outArr);
+     echo $myJSONObj;
+     //echo $outArr;
+     exit;
+}
+
+?>
+
+<html>
+<head>
+<title>Sample Insert into Gallery Form USING FETCH </title>
+<!--set some style properties::: -->
+<link rel="stylesheet" type="text/css" href="css/galleryStyle.css">
+</head>
+<body>
+ 
+<div class= "formContainer">
+<!--form done using more current tags... -->
+<form id="insertGallery" action="" enctype ="multipart/form-data">
+<!-- group the related elements in a form -->
+<h3> Take the test!</h3>
+
+<fieldset>
+  <p>This is a quick personality test to know what type of virtual persona you are today!</p>
+<p><label>Username:</label><input type="text" size="24" maxlength = "40" name = "a_name" required></p>
+<fieldset>
+    <legend>Do you agree to the terms?</legend>
+    <label><input type="radio" name="terms" value="yes" /> Yes</label>
+    <label><input type="radio" name="terms" value="no" /> No</label>
+  </fieldset>
+  <fieldset>
+    <legend>What feeling does mist at night evoke?</legend>
+    <label><input type="radio" name="mist" value="scarry" /> Scarry</label>
+    <label><input type="radio" name="mist" value="calming" /> Calming</label>
+    <label><input type="radio" name="mist" value="makes me mad" /> Makes me mad</label>
+  </fieldset>
+  <fieldset>
+    <legend>What color would best describe your day so far?</legend>
+    <input type="color" name="color" id="favcolor" value="#ff0020">
+</fieldset>
+  <fieldset>
+    <legend>What do you belive is at the end of a rainbow?</legend>
+    <label><input type="radio" name="rainbow" value="end" /> The end of the rainbow</label>
+  </fieldset>
+  <fieldset>
+    <legend>How old is your best ghost friend?</legend>
+    <input type="range" min="1" max="100" value="50" class="slider" name="ghost">
+</fieldset>
+<fieldset>
+    <legend>What do the creatures in the cloud look like?</legend>
+    <label><input type="radio" name="clouds" value="spiky" /> Spiky</label>
+    <label><input type="radio" name="clouds" value="bubbly" /> Bubbly</label>
+    <label><input type="radio" name="clouds" value="pink" /> Pink</label>
+  </fieldset>
+<fieldset>
+    <legend>Do you look back at explosions?</legend>
+    <input type="range" min="1" max="100" value="50" class="slider" name="boom">
+</fieldset>
+<fieldset>
+<legend>What type of plant are you today?</legend><input type = "text" size="24" maxlength = "40"  name = "plant" required>
+</fieldset>
+
+  
+<p class = "sub"><input type = "submit" name = "submit" value = "submit my info" id ="buttonS" /></p>
+
+ </fieldset>
+
+ <div id = "nav-container">
+<a href="creatorViewport.html">GO TO CHARACTER CREATOR</a>
+</div>
+</form>
+
+
+<div id = "result"></div> 
+
+
+</div>
+
+
+
+<script>
+    window.onload = function (){
+        console.log("ready");
+
+        fetch("InsertGalleryFetch_Mod1.php?start=true")
+           .then(function (response) {
+            return response.json();
+          })
+          .then(function (jsonData) {
+          console.log(jsonData);
+          displayResponse(jsonData);
+        });
+
+        document.querySelector("#insertGallery").addEventListener("submit", function(){
+            event.preventDefault(); // stops the submit button from submitting by default
+            console.log("button clicked");
+            console.log("form has been submitted");
+
+            //part two
+            let form = document.querySelector("#insertGallery");
+             let data = new FormData(form);
+                /*console.log to inspect the data */
+            for (let pair of data.entries()) {
+             console.log(pair[0]+ ', ' + pair[1]);
+            }
+
+            fetch('InsertGalleryFetch_Mod1.php', {
+              
+    method: 'POST',
+    body: data
+    })
+    .then(function (response) {
+            console.log("got here with response ...");
+            return response.json();
+     })
+    .then(result => {
+      console.log(result);
+ 
+        // console.log(result.response);
+        // if(result.response!=="success"){
+        //     throw new Error('Something went wrong '+ result);
+ 
+        // }
+        // else{
+        //     console.log('Here:', result);
+            displayResponse(result);
+        //     form.reset();
+ 
+        // }
+     })
+    .catch(error => {
+    console.error('Error:', error);
+    });
+
+
+        });
+
+    function displayResponse(theResult){
+      document.querySelector("#result").innerHTML="";
+
+    for(let i=0; i<theResult.length; i++){
+
+      let container = document.createElement("div");
+          container.classList.add("outer");
+          document.querySelector("#result").appendChild(container);
+    
+          let title = document.createElement("h3");
+          title.textContent= "Results from users";
+          container.appendChild(title);
+    
+          let contentContainer = document.createElement("div");
+          contentContainer.classList.add("content");
+          container.appendChild(contentContainer);
+
+      let entryObject = document.createElement("div");
+        contentContainer.appendChild(entryObject);
+
+      for (let property in theResult[i]) {
+        console.log(theResult[i][property]);
+        let content = document.createElement("p");
+        content.innerHTML = theResult[i][property];
+        entryObject.appendChild(content);
+
+      }
+    }
+
+ }
+
+    }// window onLoad
+</script>
+<a href="url">link text</a>
+</body>
+</html>
---
+++ project-V2/threejs-demo/threejs-Texturevariant-Noemie/app.js
@@ -0,0 +1,226 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/GLTFLoader.js";
+import { RGBELoader } from "three/addons/RGBELoader.js";
+import { OrbitControls } from "three/addons/OrbitControls.js";
+import { FirstPersonControls } from "three/addons/FirstPersonControls.js";
+import { GUI } from "three/addons/libs/lil-gui.module.min.js";
+
+window.onload = function () {
+
+	let scene;
+	let camera;
+	let renderer;
+	let model;
+	let gui;
+  const state = { variant: "Default" };
+
+  const MODEL_PATH = "3dassets/body.glb";
+
+  init();
+
+  //select variant from the tutorial
+  function selectVariant(scene, parser, extension, variantName) {
+    const variantIndex = extension.variants.findIndex((v) =>
+      v.name.includes(variantName)
+    );
+
+    scene.traverse(async (object) => {
+      if (!object.isMesh || !object.userData.gltfExtensions) return;
+
+      const meshVariantDef =
+        object.userData.gltfExtensions["KHR_materials_variants"];
+
+      if (!meshVariantDef) return;
+
+      if (!object.userData.originalMaterial) {
+        object.userData.originalMaterial = object.material;
+      }
+
+      const mapping = meshVariantDef.mappings.find((mapping) =>
+        mapping.variants.includes(variantIndex)
+      );
+
+      if (mapping) {
+        object.material = await parser.getDependency(
+          "material",
+          mapping.material
+        );
+        parser.assignFinalMaterial(object);
+      } else {
+        object.material = object.userData.originalMaterial;
+      }
+
+      render();
+    });
+  }
+
+
+
+//-----------------Initialize-----------------
+function init() {
+	const canvas = document.querySelector("#c");
+
+//----------------Scene, Renderer-----------------
+scene = new THREE.Scene();                    //create a scene
+scene.background = new THREE.Color(0xaaaaaa);
+//scene.fog = new THREE.Fog(backgroundColor, 60, 100);
+
+renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+renderer.shadowMap.enabled = true;
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+//----------------------Camera------------------
+camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); //create perspectivecamera Constructor(Field of view, aspect ratio, near plane, far plane) will define view frustum
+camera.position.z = 5;
+camera.position.x = 0;
+camera.position.y = 3;
+renderer = new THREE.WebGLRenderer();         //Does the math for you :) (some optional parameters)
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );   //domElement corresponds to the canvas
+
+//---------------------Helpers--------------------
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+
+//----------------------Light-------------------
+let ambientLight = new THREE.AmbientLight(0x101030, 1.0);
+scene.add(ambientLight);
+let hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 1.0);
+hemiLight.position.set(0, 50, 0);
+scene.add(hemiLight);
+
+//From Sabine's example... not sure how to change it.
+let d = 8.25;
+let dirLight = new THREE.DirectionalLight(0xffffff, 5.0);
+dirLight.position.set(-8, 10, 8);
+dirLight.castShadow = true;
+dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+dirLight.shadow.camera.near = 0.1;
+dirLight.shadow.camera.far = 1500;
+dirLight.shadow.camera.left = d * -1;
+dirLight.shadow.camera.right = d;
+dirLight.shadow.camera.top = d;
+dirLight.shadow.camera.bottom = d * -1;
+scene.add(dirLight);   // Add directional Light to scene
+
+
+//--------------
+//setEnvironment();
+loadModel();
+update();
+}//init
+
+
+//---------------------GEOMETRIES---------------------
+  //to set up environment
+  function setEnvironment() {
+    new RGBELoader()
+      .setDataType(THREE.HalfFloatType)
+      .setPath("3dassets")
+      .load("body.gltf", function (texture) {
+        texture.mapping = THREE.EquirectangularReflectionMapping;
+
+        scene.environment = texture;
+      });
+  }
+
+//to load the model
+function loadModel() {
+    let loader = new GLTFLoader();
+
+    loader.load(MODEL_PATH, function (gltf) {
+      // GUI
+      gui = new GUI();
+
+      model = gltf.scene;
+      model.scale.set(1, 1, 1);
+
+      model.position.set(0,0,0);
+      scene.add(model);
+
+      // Details of the KHR_materials_variants extension used here can be found below
+      // https://github.com/KhronosGroup/glTF/tree/master/extensions/2.0/Khronos/KHR_materials_variants
+      const parser = gltf.parser;
+
+      let variantsExtension;
+
+      if ("gltfExtensions" in gltf.userData) {
+        variantsExtension =
+          gltf.userData.gltfExtensions["KHR_materials_variants"];
+      }
+
+      if (variantsExtension != null) {
+        const variants = variantsExtension.variants.map(
+          (variant) => variant.name
+        );
+        const variantsCtrl = gui
+          .add(state, "variant", variants)
+          .name("Variant");
+
+        selectVariant(scene, parser, variantsExtension, state.variant);
+
+        variantsCtrl.onChange((value) =>
+          selectVariant(scene, parser, variantsExtension, value)
+        );
+      }
+
+      //const texture = new THREE.TextureLoader().load("waterdudv.jpg");
+      //  texture.wrapS = THREE.RepeatWrapping;
+      //   texture.wrapT = THREE.RepeatWrapping;
+      //    texture.repeat.set( 100, 100 );
+
+      //let material = new THREE.MeshBasicMaterial({map:texture});
+
+      //let mat = new THREE.MeshPhongMaterial({ map: texture });
+      render();
+    });
+  } //load model
+
+
+
+//-------------Render-------------
+  function render() {
+    renderer.render(scene, camera);
+  }
+
+  //-------------Update---------------
+  function update() {
+    // in case resize window
+    if (resizeRendererToDisplaySize(renderer)) {
+      console.log("resize");
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+    render();
+    console.log("in update");
+  } //update
+
+  //resize for window
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    let canvasPixelWidth = canvas.width / window.devicePixelRatio;
+    let canvasPixelHeight = canvas.height / window.devicePixelRatio;
+
+    const needResize =
+      canvasPixelWidth !== width || canvasPixelHeight !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  } //resize
+
+// // //not rendered yet
+//  function animate() {
+//  	requestAnimationFrame( animate );            //wait for screen refresh, call animate again
+// //     cube.rotation.x += 0.01;                     //Rotation is a function inside mesh?
+// //     cube.rotation.y += 0.01;
+// //     horn.rotation.y += 0.01;
+//  	renderer.render( scene, camera );            //render!!!
+// }
+//  animate();
+}; //load
